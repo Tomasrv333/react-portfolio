0adb837c8e3935cca9dd331005cc44cef97a9f2a
Tomasrv333 <tomasrv123@gmail.com>
2023-01-27 18:53:06 -0500

    Footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import Link from 'react-router-dom'
+import * as Unicons from '@iconscout/react-unicons'
 
 const Footer = () => {
     return (
         <footer className='footer__container'>
-            <p>footer</p>
+            <div className='footer__info'>
+            <h3>Tomas Rios Vargas</h3>
+                    <p>Desarrollador Full Stack con casi 2 años de experiencia en el desarrollo de sitios y aplicaciones web utilizando diferentes tecnologias como Javascript, Nodejs, Express, React o MongoDB</p>
+            </div>
+            <div className='contactIcons__content'>
+                <div className='icon__block'>
+                    <Unicons.UilEnvelopeAlt />
+                </div>
+                <div className='icon__block'>
+                    <Unicons.UilGithubAlt />
+                </div>
+                <div className='icon__block'>
+                    <Unicons.UilLinkedinAlt />
+                </div>
+                <div className='icon__block'>
+                    <Unicons.UilFacebookF />
+                </div>
+                <div className='icon__block'>
+                    <Unicons.UilInstagram />
+                </div>
+            </div>
+            <div className='legalTerms__content'>
+                <a>©2023 | Todos los derechos reservados</a>
+            </div>
         </footer>
     )
 }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,12 +1,13 @@
 const Hero = () => {
     return (
         <div className="hero__container">
-                <div className="hero__title">
+                <div className="hero__info">
                     <h1>Tomas Rios Vargas</h1>
-                    <h2>Full Stack Developer</h2>
+                    <h3>Desarrollador Full-Stack</h3>
                     <p>Aliquam a arcu facilisis orci volutpat rutrum ac at mauris. Nam nisl dolor, dictum et pretium et, viverra vel diam. Suspendisse vehicula cursus efficitur. Nulla mattis, felis eget molestie bibendum, augue metus fringilla felis, vitae semper erat nibh bibendum dolor.</p>
+                    
                 </div>
-                <div className="hero__desc">
+                <div className="hero__bg">
                     <p>lorem</p>
                 </div>
             </div>
