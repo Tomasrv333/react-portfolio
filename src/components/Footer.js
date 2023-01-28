e868bc2f83d724906ae229dd4a670d2fa022bf55
Tomasrv333 <tomasrv123@gmail.com>
2023-01-27 19:21:42 -0500

    footer links and functions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import Copy from "react-copy-to-clipboard"
 import * as Unicons from '@iconscout/react-unicons'
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 const Footer = () => {
+
+    const Links = {
+        gmail: 'tomasrv123@gmail.com',
+        phone: '+573214332191',
+        github: 'https://github.com/Tomasrv333',
+        linkedin: 'https://www.linkedin.com/in/tomasrvar/',
+        facebook: 'https://www.facebook.com/tomasrv333',
+        instagram: 'https://www.instagram.com/tomas_rios3/'
+    }
+
     return (
         <footer className='footer__container'>
             <div className='footer__info'>
@@ -10,21 +21,28 @@ const Footer = () => {
                     <p>Desarrollador Full Stack con casi 2 años de experiencia en el desarrollo de sitios y aplicaciones web utilizando diferentes tecnologias como Javascript, Nodejs, Express, React o MongoDB</p>
             </div>
             <div className='contactIcons__content'>
-                <div className='icon__block'>
+                <a className='icon__block' onClick={() => {
+                    navigator.clipboard.writeText(Links.phone)
+                }}>
+                    <Unicons.UilPhoneAlt />
+                </a>
+                <a className='icon__block' onClick={() => {
+                    navigator.clipboard.writeText(Links.gmail)
+                }}>
                     <Unicons.UilEnvelopeAlt />
-                </div>
-                <div className='icon__block'>
+                </a>
+                <a className='icon__block' href={Links.github} target='_blank'>
                     <Unicons.UilGithubAlt />
-                </div>
-                <div className='icon__block'>
+                </a>
+                <a className='icon__block' href={Links.linkedin} target='_blank'>
                     <Unicons.UilLinkedinAlt />
-                </div>
-                <div className='icon__block'>
+                </a>
+                <a className='icon__block' href={Links.facebook} target='_blank'>
                     <Unicons.UilFacebookF />
-                </div>
-                <div className='icon__block'>
+                </a>
+                <a className='icon__block' href={Links.instagrama} target='_blank'>
                     <Unicons.UilInstagram />
-                </div>
+                </a>
             </div>
             <div className='legalTerms__content'>
                 <a>©2023 | Todos los derechos reservados</a>
