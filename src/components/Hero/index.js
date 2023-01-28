b5953b1b2aa1abfed8e64d6d5f5c4a27be213652
Tomasrv333 <tomasrv123@gmail.com>
2023-01-27 20:25:34 -0500

    hero complements

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,16 +1,20 @@
+import * as Unicons from '@iconscout/react-unicons'
+
 const Hero = () => {
     return (
         <div id="home" name="home" className="hero__container">
-                <div className="hero__info">
-                    <h1>Tomas Rios Vargas</h1>
-                    <h3>Desarrollador Full-Stack</h3>
-                    <p>Aliquam a arcu facilisis orci volutpat rutrum ac at mauris. Nam nisl dolor, dictum et pretium et, viverra vel diam. Suspendisse vehicula cursus efficitur. Nulla mattis, felis eget molestie bibendum, augue metus fringilla felis, vitae semper erat nibh bibendum dolor.</p>
-                    
-                </div>
-                <div className="hero__bg">
-                    <p>lorem</p>
-                </div>
+            <div className="hero__info">
+                <h1>Tomas Rios Vargas</h1>
+                <h3><Unicons.UilAngleRight /> Desarrollador Full-Stack</h3>
+                <p>Aliquam a arcu facilisis orci volutpat rutrum ac at mauris. Nam nisl dolor, dictum et pretium et, viverra vel diam. Suspendisse vehicula cursus efficitur. Nulla mattis, felis eget molestie bibendum, augue metus fringilla felis, vitae semper erat nibh bibendum dolor.</p>
+                <button className='info__button'>
+                    Conoceme!
+                </button>
             </div>
+            <div className="hero__bg">
+                <p>lorem</p>
+            </div>
+        </div>
     )
 }
 
