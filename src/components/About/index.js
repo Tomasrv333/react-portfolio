bae05fddd858857c6f4914540f00503eb7a04f27
Tomasrv333 <tomasrv123@gmail.com>
2023-01-29 18:15:45 -0500

    some modifications

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,9 +2,21 @@ const About = () => {
     return (
         <section id="about" className="about__container">
             <h1>About me</h1>
-            <p>¡Hola! ✌ Soy un programador Full Stack con casi 2 años de experiencia en el desarrollo y el despliegue de sitios web utilizando varias tecnologías de Código abierto como NodeJs, así como otros sistemas de gestión de contenido como Wordpress. Además, he trabajado en diferentes proyectos comerciales para algunos clientes como Freelance utilizando más que todo tecnologías Front-End como Sass, Bootstrap y ReactJs. </p>
-            <p>Tengo una amplia experiencia en HTML, CSS, Javascript y otras tecnologías de código abierto como NodeJs, ExpressJs, ReactJs y MongoDB 💻🧭.Me encuentro practicando a diario el uso de estas tecnologías para el desarrollo de aplicaciones web basadas en una arquitectura SPA (Single Page Application). también cuento con habilidades para escribir consultas SQL complejas utilizando servidores de bases de datos populares como MySQL. Además, cuento con experiencia en controladores de versiones como GitHub.A parte de mi trabajo como desarrollador, también soy estudiante en ingeniería de Sistemas 📚 (Cursando 5° semestre) y dedico gran parte de mi tiempo en mejorar mi nivel de inglés 🚀 (Actualmente B1+). </p>
-            <p>Me considero una persona muy tranquila y respetuosa, sobre todo porque me gusta escuchar a las personas; sus ideas y experiencias. 🌞</p>
+
+            <div className="about__content">
+                <div className="about__info-content">
+                    <p>¡Hola! ✌ Soy un programador Full Stack con casi 2 años de experiencia en el desarrollo y el despliegue de sitios web utilizando varias tecnologías de Código abierto como NodeJs, así como otros sistemas de gestión de contenido como Wordpress. Además, he trabajado en diferentes proyectos comerciales para algunos clientes como Freelance utilizando más que todo tecnologías Front-End como Sass, Bootstrap y ReactJs. </p>
+                    <p>Tengo una amplia experiencia en HTML, CSS, Javascript y otras tecnologías de código abierto como NodeJs, ExpressJs, ReactJs y MongoDB 💻🧭.Me encuentro practicando a diario el uso de estas tecnologías para el desarrollo de aplicaciones web basadas en una arquitectura SPA (Single Page Application). también cuento con habilidades para escribir consultas SQL complejas utilizando servidores de bases de datos populares como MySQL. Además, cuento con experiencia en controladores de versiones como GitHub.A parte de mi trabajo como desarrollador, también soy estudiante en ingeniería de Sistemas 📚 (Cursando 5° semestre) y dedico gran parte de mi tiempo en mejorar mi nivel de inglés 🚀 (Actualmente B1+). </p>
+                    <p>Me considero una persona muy tranquila y respetuosa, sobre todo porque me gusta escuchar a las personas; sus ideas y experiencias. 🌞</p>
+                </div>
+                <div className="about__skill-content">
+                    <p>English</p>
+                </div>
+                <div className="about__stack-content">
+                    <p>Mern Stack</p>
+                </div>
+                
+            </div>
         </section>
     )
 }
