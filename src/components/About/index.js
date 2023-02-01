7ae8eb554186a1156be769f0e37b0fc0e93f9674
Tomasrv333 <tomasrv123@gmail.com>
2023-01-31 19:19:35 -0500

    style update

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,53 +2,54 @@ import Icons from '.'
 
 const About = () => {
     return (
-        <section id="about" className="about__container">
-            <h1>About me</h1>
+        <section id="about" className="about-container">
+            <h1 className='about-container__title'>About me</h1>
 
-            <div className="about__content">
-                <div className="about__info-content">
-                    <p>¡Hola! ✌ Soy un programador Full Stack con casi 2 años de experiencia en el desarrollo y el despliegue de sitios web utilizando varias tecnologías de Código abierto como NodeJs, así como otros sistemas de gestión de contenido como Wordpress. Además, he trabajado en diferentes proyectos comerciales para algunos clientes como Freelance utilizando más que todo tecnologías Front-End como Sass, Bootstrap y ReactJs. </p>
+            <div className="about-container__l-block">
+                <p className="l-block__text">¡Hola! ✌ Soy un programador Full Stack con casi 2 años de experiencia en el desarrollo y el despliegue de sitios web utilizando varias tecnologías de Código abierto como NodeJs, así como otros sistemas de gestión de contenido como Wordpress. Además, he trabajado en diferentes proyectos comerciales para algunos clientes como Freelance utilizando más que todo tecnologías Front-End como Sass, Bootstrap y ReactJs. </p>
+            </div>
+
+            <div className="about-container__r-block">
+                <div className='r-block__img'>
+                    {/* <img className='img-profile' src='/images/img-profile.jpeg'></img> */}
                 </div>
-                <div className="about__profile-content">
-                    <div className='img-profile__content'>
-                        {/* <img className='img-profile' src='/images/img-profile.jpeg'></img> */}
-                        <div>
-                            <button>Descarga CV</button>
-                        </div>
-                    </div>
-                </div>
-                <div className="about__skill-content">
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-js.svg"></img>
-                        <p>Javascript</p>
-                    </div>
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-node.svg"></img>
-                        <p>Node</p>
-                    </div>
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-express.svg"></img>
-                        <p>Express</p>
-                    </div>
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-react.svg"></img>
-                        <p>React</p>
-                    </div>
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-mongo.svg"></img>
-                        <p>Mongo</p>
-                    </div>
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-mysql.svg"></img>
-                        <p>MySQL</p>
-                    </div>
-                    <div className='skill-card'>
-                        <img className='about__icon' src="/images/icons/icon-postgresql.svg"></img>
-                        <p>Postgresql</p>
-                    </div>
+                <div className='r-block__button'>
+                    <button>Descarga CV</button>
                 </div>
                 
             </div>
+
+            <div className="about-container__b-block">
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-js.svg"></img>
+                    <p className='skill-card__text'>Javascript</p>
+                </div>
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-node.svg"></img>
+                    <p className='skill-card__text'>Node</p>
+                </div>
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-express.svg"></img>
+                    <p className='skill-card__text'>Express</p>
+                </div>
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-react.svg"></img>
+                    <p className='skill-card__text'>React</p>
+                </div>
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-mongo.svg"></img>
+                    <p className='skill-card__text'>Mongo</p>
+                </div>
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-mysql.svg"></img>
+                    <p className='skill-card__text'>MySQL</p>
+                </div>
+                <div className='b-block__skill-card'>
+                    <img className='skill-card__icon' src="/images/icons/icon-postgresql.svg"></img>
+                    <p className='skill-card__text'>Postgresql</p>
+                </div>
+            </div>
+
         </section>
     )
 }
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -2,17 +2,37 @@ import * as Unicons from '@iconscout/react-unicons'
 
 const Hero = () => {
     return (
-        <div id="home" name="home" className="hero__container">
-            <div className="hero__info">
-                <h1>Tomas Rios Vargas</h1>
-                <h3><Unicons.UilAngleRight /> Desarrollador Full-Stack</h3>
-                <p>Aliquam a arcu facilisis orci volutpat rutrum ac at mauris. Nam nisl dolor, dictum et pretium et, viverra vel diam. Suspendisse vehicula cursus efficitur. Nulla mattis, felis eget molestie bibendum, augue metus fringilla felis, vitae semper erat nibh bibendum dolor.</p>
-                <button className='info__button'>
-                    Conoceme!
-                </button>
+        <div id="home" name="home" className="hero-container"
+            style={{
+                backgroundImage: `url('https://res.cloudinary.com/djkjxhprl/image/upload/v1675194467/react-portfolio-background.jpg')`
+            }}
+        >
+            <div className="hero-container__info-block">
+                <span className='info-block__span'>Hola! Mi nombre es</span>
+                <h1 className='info-block__title'>Tomas Rios Vargas</h1>
+                <h3 className='info-block__subtitle'><Unicons.UilAngleRight /> Desarrollador Full-Stack</h3>
+                <p className='info-block__text'>Aliquam a arcu facilisis orci volutpat rutrum ac at mauris. Nam nisl dolor, dictum et pretium et, viverra vel diam. Suspendisse vehicula cursus efficitur. Nulla mattis, felis eget molestie bibendum, augue metus fringilla felis, vitae semper erat nibh bibendum dolor.</p>
+                <div className='info-block__bottom-element'>
+                    <button className='bottom-element__button'>
+                        Conoceme!
+                    </button>
+                    <span className='bottom-element__span'>Desliza hacia abajo<Unicons.UilMouseAlt /></span>
+                </div>
             </div>
-            <div className="hero__bg">
-                <p>lorem</p>
+
+            <div className="hero-container__icons-block">
+                <div className='icons-block__icon'>
+                    <Unicons.UilEnvelope />
+                </div>
+                <div className='icons-block__icon'>
+                    <Unicons.UilPhone />
+                </div>
+                <div className='icons-block__icon'>
+                    <Unicons.UilFileDownload />
+                </div>
+                <div className='icons-block__line'>
+
+                </div>
             </div>
         </div>
     )
