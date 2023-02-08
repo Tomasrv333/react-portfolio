5c79582fcc03d59dc53809fecabe5bfdfcb11716
Tomasrv333 <tomasrv123@gmail.com>
2023-02-07 21:23:24 -0500

    style fixed

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,24 +1,26 @@
 import * as Unicons from '@iconscout/react-unicons'
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 const Hero = () => {
     return (
         <div id="home" name="home" className="hero-container">
             <div className="hero-container__info-block">
-                {/* <span className='info-block__span'>Hola! Mi nombre es</span> */}
-                <h1 className='info-block__title'>Tomás Rios Vargas</h1>
-                <div className='info-block__tag'>
-                    <hr className='tag__hr'></hr>
-                    <span className='tag__span'>Desarrollador Full Stack</span>
-                    <hr className='tag__hr'></hr>
-                </div>
+                <span className='title__span'>Full Stack Developer</span>
+                <h1 className='info-block__title'>Tomás Ríos Vargas</h1>
                 <p className='info-block__text'>Soy desarrollador Full Stack y llevo 2 años trabajando en el desarrollo de sitios y aplicaciones web a traves de diferentes lenguajes y frameworks como JavaScript, NodeJs, Express, ReactJs, MongoDb y MySQL</p>
                 <div className='info-block__bottom-element'>
                     <button className='bottom-element__button'>
                         Comencemos!
                     </button>
-                    <span className='bottom-element__span'>Desliza hacia abajo<Unicons.UilMouseAlt /></span>
+                    <button className='bottom-element__button--cv'>
+                        CV
+                    </button>
                 </div>
             </div>
+            <div className='hero-container__img-block'>
+                <img className='img-block__img' src='/images/background-img.png'></img>
+            </div>
+            
         </div>
     )
 }
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -7,21 +7,29 @@ const Projects = () => {
             <AnimationOnScroll animateIn='animate__fadeIn'>
                 <h1 className="projects__title">Proyectos</h1>
                 <div className="projects__content">
-                    <div className='projects__card--main'>
-
-                    </div>
-                    <div className='projects__card--info'>
-                        <h3>Descubre todos mis proyectos!</h3>
-                        <p>Descubre un poco de mi trabajo y experiencia durante los ultimos años en donde hago uso de  tecnologias para el desarrollo de aplicaciones web. </p>
-                        <button>Github<Unicons.UilGithubAlt /></button>
+                    <div className='projects__card'>
+                        <Unicons.UilFolderOpen />
+                        <h3 className='card__title'>Task App</h3>
+                        <p className='card__text'>
+                            Aplicacion de tareas con metodologia CRUD
+                        </p>
+                        <img className='card__img' src='/images/project-1.png'></img>
+                        <span className='card__span'>NodeJs | Express | ReactJs | MongoDB</span>
+                        <div className='card__button-block'>
+                            <button className='button-block--outline'>Despliegue</button>
+                            <button className='button-block--fill'><Unicons.UilGithubAlt/></button>
+                        </div>
                     </div>
                     <div className='projects__card'>
+                        <Unicons.UilFolderOpen />
 
                     </div>
                     <div className='projects__card'>
+                        <Unicons.UilFolderOpen />
 
                     </div>
-                    <div className='projects__card--more'>
+                    <div className='projects__card'>
+                        <Unicons.UilFolderOpen />
 
                     </div>
                 </div>
