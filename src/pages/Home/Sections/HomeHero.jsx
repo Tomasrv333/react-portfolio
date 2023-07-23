3e97ca258c457f4229d72a7088f71922f4621f22
Tomasrv333 <tomasrv123@gmail.com>
2023-07-22 20:46:07 -0500

    Responsive design success

--- src/pages/Home/Sections/HomeHero.jsx
+++ src/pages/Home/Sections/HomeHero.jsx
@@ -8,10 +8,19 @@ const HomeHero = () => {
     <div className="container">
       <div className="hero__content">
         <div className="hero__info">
-          <h1 className="hero__title">Tomás Ríos Vargas<span>Full Stack Developer</span></h1>
-          <span className="hero__span">Welcome to my portfolio...</span>
-          <hr className='hero__line'></hr>
-          <p className="hero__description">I will create custom web solutions for your needs. With expertise in full stack development, Ill deliver functional and visually appealing websites and applications that provide a seamless user experience.</p>
+          <div className='hero__info--up-group'>
+            <img src={ImageProfile} alt="Tomás Ríos Vargas" />
+            <div className='hero__info--title-group'>
+              <span className="hero__span">Welcome, I'm</span>
+              <h1 className="hero__title">Tomás Ríos Vargas</h1>
+              <h2 className='hero__subtitle'>Full Stack Developer</h2>
+              <p className="hero__description">I will create custom web solutions for your needs. With expertise in full stack development, Ill deliver functional and visually appealing websites and applications that provide a seamless user experience.</p>
+              <div className='hero__copyEmail'>
+                <span>tomasrv123@gmail.com</span>
+                <FaCopy/>
+              </div>
+            </div>
+          </div>
           <button className='hero__btn button--primary'><a href="#contact">Contact me</a> </button>
         </div>
         <div className="hero__side">
@@ -20,10 +29,6 @@ const HomeHero = () => {
           </div>
           <div className="hero__media">
             <SocialMedia/>
-          </div>
-          <div className='hero__copyEmail'>
-            <span>tomasrv123@gmail.com</span>
-            <FaCopy/>
           </div>
         </div>
       </div>
